1445576f9f7f913e72c4170ac3cce24b1ba412d8
Elias <elias.ortizreyes@gmail.com>
2017-01-29 21:35:36 +0100

    Added Google Maps fragments, and used API in config file (not commited, due sensitive value)

--- app/src/main/java/com/eliasor/earthquakeinfo/MainActivity.java
+++ app/src/main/java/com/eliasor/earthquakeinfo/MainActivity.java
@@ -3,15 +3,27 @@ package com.eliasor.earthquakeinfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private FirebaseAnalytics mFirebaseAnalytics;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 }
