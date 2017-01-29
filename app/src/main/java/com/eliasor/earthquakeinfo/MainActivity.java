811608f656039e0068027602b796a0ba4530b43a
Elias <elias.ortizreyes@gmail.com>
2017-01-29 19:49:53 +0100

    Enabling firebase analytics, and services google maps

--- app/src/main/java/com/eliasor/earthquakeinfo/MainActivity.java
+++ app/src/main/java/com/eliasor/earthquakeinfo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.eliasor.earthquakeinfo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
